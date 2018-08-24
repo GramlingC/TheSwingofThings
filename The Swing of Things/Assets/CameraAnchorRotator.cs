93a61bfab7013d758fa5446f4282b4575251a27f
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2018-08-24 11:08:15 -0700

    Got camera to work how I wanted it to work.

--- The Swing of Things/Assets/CameraAnchorRotator.cs
+++ The Swing of Things/Assets/CameraAnchorRotator.cs
@@ -5,30 +5,34 @@ using UnityEngine;
 public class CameraAnchorRotator : MonoBehaviour {
     public float horizontalSensitivity;
     public float verticalSensitivity;
-    private Quaternion minStep = new Quaternion();
+    public float angleFromTrueTop;
+
     private float xAxis, yAxis;
 	// Use this for initialization
 	void Start () {
-        minStep.eulerAngles = new Vector3(0, horizontalSensitivity, 0);
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyDown(KeyCode.Escape)) {
+        /*if(Input.GetKey(KeyCode.Escape)) {
           Cursor.lockState = CursorLockMode.None;
           Cursor.visible=true;
-        }
+        }*/
         xAxis = Input.GetAxisRaw("Mouse X");
         yAxis = Input.GetAxisRaw("Mouse Y");
         Vector3 rot = transform.rotation.eulerAngles;
         rot.x -= verticalSensitivity*yAxis;
         rot.y += horizontalSensitivity*xAxis;
-        float minAngle = 180;
-        float maxAngle = 180;
-        if (rot.x > minAngle && rot.x < 90) rot.x = minAngle;
-        if (rot.x > 360 - 90 && rot.x < 360 - maxAngle) rot.x = 360 - maxAngle;
+        rot.z = 0;
+        
+        Vector3 currentRot = Quaternion.Euler(rot.x, rot.y, rot.z) * new Vector3(0, 0, 1);
+        //if change just applied to rot is deemed to be too close to true top or true bottom, undo that invalid change
+        if ((Vector3.Angle(currentRot, new Vector3(0, 1, 0)) < angleFromTrueTop) || (Vector3.Angle(currentRot, new Vector3(0, -1, 0)) < angleFromTrueTop))
+        {
+            rot.x += verticalSensitivity * yAxis;
+        }
         transform.rotation = Quaternion.Euler(rot.x, rot.y, rot.z);
-	}
+    }
 }
--- The Swing of Things/Assets/MinimalPlayerMovement.cs
+++ The Swing of Things/Assets/MinimalPlayerMovement.cs
@@ -9,7 +9,6 @@ public class MinimalPlayerMovement : MonoBehaviour {
     public float maxWalkingSpeed;
     public float friction;
     public GameObject debug;
-    private string output = "";
 	// Use this for initialization
 	void Start () {
         velocity = new Vector3();
@@ -29,19 +28,13 @@ public class MinimalPlayerMovement : MonoBehaviour {
         }
         //get movement input this frame
         Vector3 thisFramesInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        output += thisFramesInput.ToString();
         //edit to be relative to default rotation, not current view
         thisFramesInput = transform.rotation * thisFramesInput;
-        
-        
-        //rotation could have rotated a 2d plane input into z component, dont want walking input to allow one to move up or down, so setting that back to 0
+        //rotation could have rotated a 2d plane input into y component, dont want walking input to allow one to move up or down, so setting that back to 0
         thisFramesInput.y = 0;
-        output += thisFramesInput.ToString();
         //changing magnitude of input to match movementSpeed so that the entire vector represents movement from walking this frame
         thisFramesInput = thisFramesInput.normalized * movementSpeed;
         debug.transform.position = transform.position + thisFramesInput;
-        if(thisFramesInput.y != 0)
-            Debug.Log(thisFramesInput.ToString());
 
         //attempt to apply movementSpeed velocity change
         velocity += thisFramesInput;
@@ -54,8 +47,5 @@ public class MinimalPlayerMovement : MonoBehaviour {
         //apply positional change
         //gameObject.transform.Translate(Time.deltaTime * velocity);
         transform.position = transform.position + Time.deltaTime * velocity;
-        if(thisFramesInput.magnitude > 0)
-        //Debug.Log(output);
-        output = "";
 	}
 }
